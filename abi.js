be4eee4ebb6a10b3befbec29627fe43ea74d7d99
laynath242@gmail.com <laynath242@gmail.com>
2021-09-23 21:45:37 +0700

    remove

--- abi.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const contract = JSON.parse(fs.readFileSync('./solidity-contract/build/contracts/SELToken.json', 'utf8'));
-// console.log(JSON.stringify(contract.abi));
-console.log(JSON.stringify(contract.bytecode));
-
